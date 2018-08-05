906d225a58069a2ea2e213c67ec683a2cacf348a
dj_segfault <david@thekramers.net>
2018-08-05 19:40:05 -0400

    Removed missing source files and got rid of LightBrightMain directory

---
+++ lightbright/SerialHandler.h
@@ -0,0 +1,31 @@
+/*
+  MIT License   Copyright (c) 2016 David Kramer
+  LightBrightLib - Library for controlling lights.
+  https://github.com/djsegfault/arduino/wiki/Project-LightBright
+  
+  Serialhandler.h
+  Handles serial communications
+  NOTE This is a stub implementation, 
+  Eventually I will have a command processor and DSM this will call
+*/
+
+#ifndef SerialHandler_h
+#define SerialHandler_h
+
+#include "Arduino.h"
+#include "LightBrightConfig.h"
+#include "LightBoard.h"
+#include "Channel.h"
+
+
+class SerialHandler
+{
+	public:
+		void begin(LightBoard *lightBoard);
+		bool handleSerial();
+		bool handleKey(char key);
+	protected:
+		LightBoard *_lightBoard;
+};	
+
+#endif
