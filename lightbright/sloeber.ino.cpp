825ba39c3f2950bc9ce8e6086a33f0f0821f1d60
dj_segfault <david@thekramers.net>
2018-08-05 19:50:02 -0400

    Got it working under Eclipse with Sloeber Arduino plugingit status .

---
+++ lightbright/sloeber.ino.cpp
@@ -0,0 +1,20 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2018-08-05 19:46:42
+
+#include "Arduino.h"
+#include <PS2Keyboard.h>
+#include "LightBrightConfig.h"
+#include "DigitalOutPin.h"
+#include "AnalogOutPin.h"
+#include "Channel.h"
+#include "KeyboardHandler.h"
+#include "SerialHandler.h"
+#include "LightBoard.h"
+#include <Logging.h>
+
+#include "LightBrightMain.ino"
+
+#endif
