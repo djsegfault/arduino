1856cee3cf33629da33aaf667a46b80fbd74219a
david.kramer <david@thekramers.net>
2019-07-10 02:45:38 -0400

    Changed display reset pin to 6 (A1) apparently that works, even A1 works go figure

--- cyberbracer/cpx_oled_test/cpx_oled_test.ino
+++ cyberbracer/cpx_oled_test/cpx_oled_test.ino
@@ -8,16 +8,6 @@
 #include <Adafruit_SSD1306.h>
 
 
-//// I2C
-#define OLED_I2C_ADDRESS 0x3C
-
-//// OLED
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 //// BUTTONS
 int CAP_THRESHOLD = 450;
 const uint8_t BTN_UP = 0;     //readCap for A6
@@ -27,50 +17,70 @@ const uint8_t BTN_BACK = 10;  //readCap for A3
 const uint8_t BTN_USER1 = 6;  //readCap for A1
 const uint8_t BTN_USER2 = 12;  //readCap for A0
 
+//// I2C
+#define OLED_I2C_ADDRESS 0x3C
+
+//// OLED
+#define SCREEN_WIDTH    128 // OLED display width, in pixels
+#define SCREEN_HEIGHT   64 // OLED display height, in pixels
+#define OLED_RESET      BTN_USER1 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
 //// Other
 boolean DEBUG_MODE = true;
 
+//// Functions
 void debug(char* message) {
   Serial.println(message);
   display.clearDisplay();
   display.setCursor(0, 0);
   display.println(message);
   display.display();
-
 }
+
 
 boolean isCapPressed(uint8_t capNum) {
   int capValue = CircuitPlayground.readCap(capNum, 30);
   if ( capValue > CAP_THRESHOLD) {
     CircuitPlayground.playTone(262, 10);
-      Serial.print("Cap ");
-      Serial.print(capNum);
-      Serial.print(" pressed, value is ");
-      Serial.println(capValue);
-    return true;
-  }
+    /*
     Serial.print("Cap ");
     Serial.print(capNum);
-    Serial.print(" NOT pressed, value is ");
+    Serial.print(" pressed, value is ");
     Serial.println(capValue);
+    */
+    return true;
+  }
+  /*
+  Serial.print("Cap ");
+  Serial.print(capNum);
+  Serial.print(" NOT pressed, value is ");
+  Serial.println(capValue);
+  */
   return false;
 }
 
-
+//// Main methods
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   CircuitPlayground.begin();
 
-
+  Serial.println("Initializing display");
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS)) {
     Serial.println("SSD1306 allocation failed");
     for (;;); // Don't proceed, loop forever
   }
+  Serial.println("Initialized display");
+
   // Clear the buffer
   display.clearDisplay();
-  display.setTextSize(2);
+  display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.display();
@@ -80,14 +90,14 @@ void setup() {
 }
 
 void loop() {
-    if (CircuitPlayground.rightButton()) {
-      debug("Right");
-    }
-    
-    if (CircuitPlayground.leftButton()) {
-      debug("Left");
-    }
+  //Serial.println("Top of loop");
+  if (CircuitPlayground.rightButton()) {
+    debug("Right");
+  }
 
+  if (CircuitPlayground.leftButton()) {
+    debug("Left");
+  }
 
   if (isCapPressed(BTN_UP)) {
     debug("Up");
@@ -104,11 +114,13 @@ void loop() {
   if (isCapPressed(BTN_USER1)) {
     debug("User 1");
   }
+  /*
   if (isCapPressed(BTN_USER2)) {
     debug("User 2");
   }
+  */
 
-  digitalWrite(13, 1);
-  delay(1000);
-  digitalWrite(13, 0);
+  digitalWrite(LED_BUILTIN, 1);
+  delay(200);
+  digitalWrite(LED_BUILTIN, 0);
 }
