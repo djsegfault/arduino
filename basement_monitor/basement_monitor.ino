c756b52f67d88a7c293690b1f8828a2b78463ddc
djsegfault <david@thekramers.net>
2021-01-11 02:11:42 -0500

    Display and DHT working.  Light level next

--- basement_monitor/basement_monitor.ino
+++ basement_monitor/basement_monitor.ino
@@ -4,20 +4,20 @@
 #include <TaskManager.h>
 #include <WiFi.h>
 
-/**
- * config.sh should define:
- * const char* wifiSSID = "...";
- * const char* wifiPassword = "....."
- **/
+// Remember to copy config_TEMPLATE.h to config.h and update the values
 #include "config.h"
 
-// Constants
-const int DHT_DATA_PIN = 17;
+// Constants for display locations: character width in dots and height in dots
+const int CW = 5;
+const int CH = 9;
 
 // Globals
 DHTesp dht;
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
+double temperature_f = 0;
+double humidity = 0;
+char buffer[81];
 
 void mqttConnect()
 {
@@ -26,7 +26,7 @@ void mqttConnect()
     {
         Serial.print("Attempting MQTT connection...");
         // Attempt to connect
-        if (mqttClient.connect(mqttClientName, mqttUser, mqttPassword))
+        if (mqttClient.connect(MQTT_CLIENT_NAME, MQTT_USERNAME, MQTT_PASSWORD))
         {
             Serial.println("MQTT connected");
             // Once connected, publish an announcement...
@@ -65,10 +65,10 @@ void mqttSubHandler(char *topic, byte *message, unsigned int length)
 void setupWiFi()
 {
     Serial.print("Connecting to ");
-    Serial.println(wifiSSID);
+    Serial.println(WIFI_SSID);
     WiFi.disconnect();
     WiFi.mode(WIFI_MODE_STA);
-    WiFi.begin(wifiSSID, wifiPassword);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -80,11 +80,56 @@ void setupWiFi()
     Serial.println(WiFi.localIP());
 }
 
+void setupDHTTask()
+{
+    // Read dht every 1000[ms] and repeat forever
+    Tasks.interval(10000, [] {
+        // Reading temperature for humidity takes about 250 milliseconds!
+        // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+        TempAndHumidity dhtValues = dht.getTempAndHumidity();
+        // Check if any reads failed and exit early (to try again).
+        if (dht.getStatus() != 0)
+        {
+            Serial.println("DHT11 error status: " + String(dht.getStatusString()));
+            return false;
+        }
+
+        temperature_f = (1.8 * dhtValues.temperature) + 32.0;
+        humidity = dhtValues.humidity;
+        Serial.println(temperature_f);
+        Serial.println(humidity);
+
+        // Send to MQTT
+        mqttConnect();
+        sprintf(buffer, "%2.2f", temperature_f);
+        mqttClient.publish("sensors/basement/temperature_f", buffer);
+
+        sprintf(buffer, "%2.2f", humidity);
+        mqttClient.publish("sensors/basement/humidity", buffer);
+        updateDisplay();
+
+    });
+}
+
 void setupMQTT()
 {
-    mqttClient.setServer(mqttServer, mqttPort);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
     mqttClient.setCallback(mqttSubHandler);
     mqttConnect();
+}
+
+void updateDisplay()
+{
+    Heltec.display->clear();
+    Heltec.display->drawString(0, 0, "IP");
+    sprintf(buffer, "IP %s", WiFi.localIP().toString().c_str());
+    Heltec.display->drawString(0, CH*1, MQTT_CLIENT_NAME);
+    Heltec.display->drawString(0, 0, buffer);
+    sprintf(buffer, "T %2.2f  H %2.2f", temperature_f, humidity);
+    Heltec.display->drawString(0, CH*3, buffer);
+    sprintf(buffer, "MS %ld", millis());
+    Heltec.display->drawString(0, CH*4, buffer);
+    Heltec.display->display();
 }
 
 void setup()
@@ -93,54 +138,16 @@ void setup()
     Serial.begin(115200);
     delay(2000);
 
+    // Display setup
+    Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
+
     // Sensor setup
     dht.setup(DHT_DATA_PIN, DHTesp::DHT11);
     Serial.println("DHT initiated");
 
     setupWiFi();
     setupMQTT();
-
-    // Read dht every 1000[ms] and repeat forever
-    Tasks.interval(10000, [] {
-        char buffer[8];
-        // Reading temperature for humidity takes about 250 milliseconds!
-        // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-        TempAndHumidity newValues = dht.getTempAndHumidity();
-        // Check if any reads failed and exit early (to try again).
-        if (dht.getStatus() != 0)
-        {
-            Serial.println("DHT11 error status: " + String(dht.getStatusString()));
-            return false;
-        }
-
-        double temperature_f = (1.8 * newValues.temperature) + 32.0;
-        double humidity = newValues.humidity;
-        Serial.println(temperature_f);
-        Serial.println(humidity);
-
-        // Send to MQTT
-        mqttConnect();
-        sprintf(buffer,"%2.2f", temperature_f);
-        mqttClient.publish("sensors/basement/temperature_f", buffer);
-        Serial.print("Sent T ");
-        Serial.println(buffer);
-        sprintf(buffer,"%2.2f", humidity);
-        mqttClient.publish("sensors/basement/humidity", buffer);
-        Serial.print("Sent H ");
-        Serial.println(buffer);
-
-        Serial.print("interval forever task: now = ");
-        Serial.println(millis());
-    });
-
-    Serial.print("task start: now = ");
-    Serial.println(millis());
-
-    if (!mqttClient.connected()) {
-        Serial.println("End of setup, NOT connected");
-    } else {
-        Serial.println("End of setup, still connected");
-    }
+    setupDHTTask();
 }
 
 void loop()
---
+++ basement_monitor/config_TEMPLATE.h
@@ -0,0 +1,9 @@
+// rename to config.h
+const char* WIFI_SSID = "";
+const char* WIFI_PASSWORD = "";
+const char* MQTT_HOST = "";
+const char* MQTT_CLIENT_NAME = "";
+const char* MQTT_USERNAME = "";
+const char* MQTT_PASSWORD = "";
+const int   MQTT_PORT = 1883;
+const int   DHT_DATA_PIN = 17;
